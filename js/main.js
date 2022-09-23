c6ffb27b9251eff1ab64dbf914a4f25373b1441b
Jacob Seil <jacobraulseil@gmail.com>
2022-09-22 19:51:56 -0700

    Add window resize response to catalogue, set up catalogue item selection

--- js/main.js
+++ js/main.js
@@ -11,27 +11,42 @@ const banner = document.querySelector('.banner');
 const itemCatalogue = document.querySelector('.item-catalogue');
 const galleryLinks = document.querySelectorAll('.gallery-link');
 
+let systemBusy = false;
+let windowResizing = false;
 let galleryName = '';
 let galleryItems = [];
-let catalogueItemIndex = 0;
-let catalogueItemLimit = Math.floor((window.innerWidth * 0.8) / 120); //placeholder; make dynamic with css variable & media query
+let catalogueIndex = 0;
+let catalogueItemLimit = Math.floor((window.innerWidth * 0.8) / 120);
 
-
+//timeouts
 function sleep(ms) {
    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function limitUserActions(ms) {
-   root.dataset.busy = 'true';
+   systemBusy = true;
    await sleep(ms);
-   delete root.dataset.busy;
+   systemBusy = false;
 }
 
 function getJsonData(input) {
    return input.then((data) => data.json());
 }
 
-function removeChildren(parent, numToRemove = parent.childElementCount, index = 0) {
+function endOfAnimation(element) {
+   return new Promise(resolve => {
+      element.onanimationend = () => resolve();
+   })
+}
+
+function endOfTransition(element) {
+   return new Promise(resolve => {
+      element.ontransitionend = () => resolve();
+   })
+}
+
+//misc management
+function removeChildren(parent, numToRemove = parent.childElementCount, index = parent.childElementCount - 1) {
    while (numToRemove > 0, numToRemove--) {
       parent.childNodes[index].remove();
       if (index !== 0) { index--; }
@@ -44,52 +59,91 @@ function removeLinkHighlight() {
    }
 }
 
-function updateCatalogueItemIndex() {
-   catalogueItemIndex = galleryItems.indexOf(itemCatalogue.lastChild.dataset.name) + 1;
+//catalogue controls
+async function selectCatalogueItem(catalogueItem) {
+   const itemName = catalogueItem.dataset.name;
+   const itemData = fetch(`https://api.genshin.dev/${galleryName}/${itemName}`);
+   const previousSelection = document.querySelector('.selected');
+
+   if (previousSelection) { previousSelection.classList.remove('selected'); }
+   catalogueItem.classList.add('selected');
+
+   await getJsonData(itemData).then((data) => console.log(data));
 }
 
-function populateCatalogue(referenceNode = null) {
-   let start = catalogueItemIndex;
-   let end = start + catalogueItemLimit;
+function updateCatalogueIndex() {
+   catalogueIndex = galleryItems.indexOf(itemCatalogue.lastChild.dataset.name) + 1;
+}
 
+function populateCatalogue(referenceNode = null, specifiedCount = null) {
+   let start = catalogueIndex;
+   let end = start + catalogueItemLimit;
+   
    if (referenceNode) {
-      const inverseIndex = catalogueItemIndex - catalogueItemLimit * 2;
+      const inverseIndex = catalogueIndex - catalogueItemLimit * 2;
       start = inverseIndex >= 0 ? inverseIndex : 0;
       end = inverseIndex >= 0 ? start + catalogueItemLimit : inverseIndex + catalogueItemLimit;
    }
 
-   for (const item of galleryItems.slice(start, end)) {
+   if (specifiedCount) {
+      end = start + specifiedCount;
+   }
+
+   for (const itemName of galleryItems.slice(start, end)) {
       const catalogueItem = itemCatalogue.insertBefore(document.createElement('li'), referenceNode);
       const itemImage = catalogueItem.appendChild(document.createElement('img'));
 
-      catalogueItem.dataset.name = item;
-      itemImage.setAttribute('src', `https://api.genshin.dev/${galleryName}/${item}/icon`);
+      catalogueItem.dataset.name = itemName;
+      itemImage.setAttribute('src', `https://api.genshin.dev/${galleryName}/${itemName}/icon`);
+      catalogueItem.addEventListener('click', async function() {
+         selectCatalogueItem(this);
+      })
    }
-
-   updateCatalogueItemIndex();
+   updateCatalogueIndex();
 }
 
-function endOfAnimation() {
-   return new Promise(resolve => {
-      itemCatalogue.onanimationend = () => resolve();
-   })
+function setCatalogueWidth(itemLimit) {
+   root.style.setProperty('--catalogue-width', `${itemLimit * 120}px`);
+}
+
+function resizeCatalogue() {
+   if (!itemCatalogue.firstChild) { return; }
+
+   const newItemLimit = Math.floor((window.innerWidth * 0.8) / 120);
+   const difference = Math.abs(catalogueItemLimit - newItemLimit);
+
+   if (!difference) { return; }
+
+   if (newItemLimit < catalogueItemLimit) {
+      removeChildren(itemCatalogue, difference);
+      updateCatalogueIndex();
+   } else { populateCatalogue(null, difference); }
+
+   setCatalogueWidth(newItemLimit);
+   catalogueItemLimit = newItemLimit;
 }
 
 // ----------
 // Initialize
 // ----------
-root.style.setProperty('--catalogue-width', `${catalogueItemLimit * 120}px`);
+setCatalogueWidth(catalogueItemLimit);
+
+window.onresize = () => {
+   clearTimeout(windowResizing);
+   windowResizing = setTimeout(resizeCatalogue, 250);
+}
 
 // -------
 // Go Home
 // -------
 document.querySelector('.logo').addEventListener('click', async function() {
-   if (root.dataset.busy) { return; }
+   if (systemBusy) { return; }
    limitUserActions(1000);
 
    removeLinkHighlight();
    gallery.style.opacity = '0';
    await sleep(500);
+   gallery.style.display = 'none';
    banner.style.opacity = '1';
 })
 
@@ -98,15 +152,22 @@ document.querySelector('.logo').addEventListener('click', async function() {
 // ----------------
 for (const link of galleryLinks) {
    link.addEventListener('click', async function() {
-      if (root.dataset.busy) { return; }
+      if (systemBusy) { return; }
       limitUserActions(1000);
 
       galleryName = this.dataset.name;
       const catalogueData = fetch(`https://api.genshin.dev/${galleryName}`);
       const toRemove = [
+         'aloy',
+         'ayato',
          'collei',
+         'defense-mechanism',
+         'eye-of-the-storm',
+         'fatui-skirmisher',
+         'hilichurl-shooter',
          'kuki-shinobu',
          'shikanoin-heizou',
+         'the-great-snowboar-king',
          'tighnari',
          'traveler-anemo',
          'traveler-dendro',
@@ -119,18 +180,20 @@ for (const link of galleryLinks) {
       link.classList.add('highlighted');
       banner.style.opacity = '0';
       gallery.style.opacity = '0';
+      gallery.style.display = 'flex';
       await sleep(500);
 
       //reset gallery
       removeChildren(itemCatalogue);
-      catalogueItemIndex = 0;
+      catalogueIndex = 0;
 
       //assign filtered api data
       await getJsonData(catalogueData)
-         .then((data) => galleryItems = data.filter(item => !toRemove.includes(item)));
+         .then((data) => galleryItems = data.filter(itemName => !toRemove.includes(itemName)));
 
-      //populate initial catalogue html
+      //populate initial catalogue items
       populateCatalogue();
+      selectCatalogueItem(itemCatalogue.firstChild);
 
       gallery.style.opacity = '1';
    })
@@ -143,7 +206,7 @@ const catalogueIndexers = document.querySelectorAll('[data-direction]');
 
 for (const indexer of catalogueIndexers) {
    indexer.addEventListener('click', async function() {
-      if (root.dataset.busy) { return; }
+      if (systemBusy) { return; }
       limitUserActions(1000);
 
       const direction = this.dataset.direction;
@@ -164,13 +227,13 @@ for (const indexer of catalogueIndexers) {
       itemCatalogue.dataset.animation = direction === 'back' ? 'back' : 'forward';
 
       //wait for animation to finish and reset data attribute
-      await endOfAnimation();
-      itemCatalogue.dataset.animation = 'off';
+      await endOfAnimation(itemCatalogue);
+      delete itemCatalogue.dataset.animation;
 
       //remove number of items added from opposite end of catalogue
       if (direction === 'back') { referenceIndex = totalItems - 1; }
       removeChildren(itemCatalogue, itemsAdded, referenceIndex)
-      updateCatalogueItemIndex();
+      updateCatalogueIndex();
    })
 }
 
@@ -200,11 +263,11 @@ function toggleMood() {
 }
 
 //duct-tape fix for lack of transition functionality with background-image gradients
-async function peekABoo() {
+async function peekABooGradient() {
    for(const indexer of catalogueIndexers) {
       delete indexer.dataset.gradient;
    }
-   await sleep(500);
+   await endOfTransition(document.body);
    for(const indexer of catalogueIndexers) {
       indexer.dataset.gradient = '';
    }
@@ -213,7 +276,7 @@ async function peekABoo() {
 //user controls
 for (const element of themeElements) {
    element.addEventListener('click', function() {
-      if (root.dataset.busy) { return; }
+      if (systemBusy) { return; }
       limitUserActions(500);
 
       root.dataset.theme = this.dataset.theme;
@@ -222,21 +285,21 @@ for (const element of themeElements) {
       if (root.dataset.mood === 'dim') {
          toggleMood();
       }
-      peekABoo();
+      peekABooGradient();
    })
 }
 
 document.querySelector('.lightbulb').addEventListener('click', function() {
-   if (root.dataset.busy) { return; }
+   if (systemBusy) { return; }
    limitUserActions(500);
 
    toggleMood();
-   peekABoo();
+   peekABooGradient();
    root.dataset.mood = root.dataset.mood === 'bright' ? 'dim' : 'bright';
 })
 
 document.querySelector('.theme-tab').addEventListener('click', function() {
-   if (root.dataset.busy) { return; }
+   if (systemBusy) { return; }
    limitUserActions(500);
 
    const themePanel = this.parentElement;
